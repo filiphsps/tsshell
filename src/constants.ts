eb1a4a11cb0c9c518ae1fca7384487b5bc3ba4bd
Filiph Siitam Sandström <filiph@nordcom.io>
2025-02-17 16:29:01 +0100

    Filter empty paths from PATHS constant.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const HOME = process.env.HOME ?? '';
 export const PATH = process.env.PATH ?? '';
-export const PATHS = PATH.split(':');
+export const PATHS = PATH.split(':').filter((p) => p);
